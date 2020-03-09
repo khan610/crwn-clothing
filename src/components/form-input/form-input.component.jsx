47193d07d37f3ddf9fc02fc9f0f2ee859a8ad22b
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-03-08 22:21:43 -0700

    Form Input, Sign in component and Custom Button

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './form-input.styles.scss';
 
+
 const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className="group">
         <input className='form-input' onChange={handleChange} {...otherProps}/>
@@ -16,3 +17,5 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
         )   : null}
     </div>
 )
+
+export default FormInput;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -33,18 +37,25 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} value={this.state.email} required />
-                    <label>Email</label>
-                    <input 
+                    <FormInput 
+                        name="email" 
+                        type="email" 
+                        handleChange={this.handleChange} 
+                        value={this.state.email}
+                        label="email" 
+                        required 
+                    />                    
+                    <FormInput 
                         name="password" 
                         type="password" 
                         value={this.state.email} 
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label="password"
                         required 
                     />
-                    <label>Email</label>    
+                        
                     
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit"> Sign in </CustomButton>
                 </form>
             </div>
         );
