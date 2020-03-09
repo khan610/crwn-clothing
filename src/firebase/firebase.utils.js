6082b9f5e302c5b891a91d0dbcc640ab6404316d
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-03-09 01:57:28 -0700

    Firebase and Gogle Authentication

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAGz5j213VYjM_8DTNaSpSoeOSqMSegW4o",
+    authDomain: "crwn-db-aa78c.firebaseapp.com",
+    databaseURL: "https://crwn-db-aa78c.firebaseio.com",
+    projectId: "crwn-db-aa78c",
+    storageBucket: "crwn-db-aa78c.appspot.com",
+    messagingSenderId: "472932378926",
+    appId: "1:472932378926:web:a9cabce3438bb07e6072bf",
+    measurementId: "G-2QBYL06L1X"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
