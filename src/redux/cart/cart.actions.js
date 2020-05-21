157c31b96aa0bd98b861820222cbfe1f8786c2c3
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-05-21 15:46:16 +0200

    updated shop data and cart utils

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,11 @@
 import CartActionsTypes from './cart.types';
+import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: CartActionsTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 });
