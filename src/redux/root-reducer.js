1a5bed95eb5faa0451448308e1ae14db4fd79b3f
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-05-28 23:15:20 +0200

    Persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+const persistCongif = { 
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+   user: userReducer,
+   cart: cartReducer,
+   directory: directoryReducer 
 });
+
+export default persistReducer(persistCongif, rootReducer);
