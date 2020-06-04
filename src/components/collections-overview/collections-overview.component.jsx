db1aed26372a0c36e5b148dcdeab30e24a9026f4
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-06-04 20:54:50 +0200

    hijgfhgfhgf

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -9,7 +9,7 @@ import  CollectionPreview  from '../collection-preview/collection-preview.compon
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className='collectins-overview'>
+    <div className='collections-overview'>
         { collections.map(({ id, ...otherCollectionProps }) => (
             <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
