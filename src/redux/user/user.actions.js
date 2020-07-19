497b948537ec9ae9c22b35bde279e79b31441a4e
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-07-19 23:50:11 +0200

    Update

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,37 +1,26 @@
 import  UserActionTypes from './user.types';
 
-export const setCurrentUser = user => ({
-    type: UserActionTypes.SET_CURRENT_USER,
-    payload: user
-});
-//Google Sign in
 
 export const googleSignInStart = () => ({
     type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
-export const googleSignInSuccess = user => ({
-    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+export const signInSuccess = user => ({
+    type: UserActionTypes.SIGN_IN_SUCCESS,
     payload: user
 });
 
-export const googleSignInFailure = error => ({
-    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+export const signInFailure = error => ({
+    type: UserActionTypes.SIGN_IN_FAILURE,
     payload: error
 });
-//Email Sign in
 
 export const emailSignInStart = emailAndPassword => ({
     type: UserActionTypes.EMAIL_SIGN_IN_START,
     payload: emailAndPassword
 });
 
-export const emailSignInSuccess = user => ({
-    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-    payload: user
+export const checkUserSession = () => ({
+    type: UserActionTypes.CHECK_USER_SESSION
 });
 
-export const emailSignInFailure = error => ({
-    type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-    payload: error
-});
