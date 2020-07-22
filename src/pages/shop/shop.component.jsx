8f0920933544276e885f5613545e8fa3d86b41dd
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-07-22 11:18:00 +0200

    Update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CollectionPageContainer from '../collection/collection.container';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart, fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-     const { fetchCollectionsStart} = this.props;
-
-     fetchCollectionsStart();
-  }
-
-  render() {
-    const { match } = this.props;   
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
       return (
         <div className='shop-page'>    
           <Route 
@@ -29,8 +24,8 @@ class ShopPage extends React.Component {
           />
         </div>
       );
-  }
 }
+
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
